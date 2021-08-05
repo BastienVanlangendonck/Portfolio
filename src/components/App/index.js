7ec16cf82d0baa04d39b5b790876b7b4b98bdf82
BastienVanlangendonck <bastien.vanlan@gmail.com>
2021-08-05 18:56:35 +0200

    header done mobile

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Import
 import Header from 'src/containers/Header';
+import Home from 'src/components/Home';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Home />
   </div>
 );
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const Home = () => (
+  <div className="home">
+    <h2 className="home-lastname">VANLANGENDONCK</h2>
+    <h1 className="home-firstname">BASTIEN</h1>
+    <div className="home-bar1" />
+    <div className="home-bar2" />
+    <h3 className="home-dev">FrontEnd Web Developer</h3>
+    <p className="home-react">Main React</p>
+  </div>
+);
+
+export default Home;
