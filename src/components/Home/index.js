162c71f77079d4546109efcf44c63f038a54ba2f
BastienVanlangendonck <bastien.vanlan@gmail.com>
2021-08-15 19:10:21 +0200

    desktop done without skills

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,8 +7,10 @@ const Home = () => (
     <h1 className="home-firstname">BASTIEN</h1>
     <div className="home-bar1" />
     <div className="home-bar2" />
-    <h3 className="home-dev">FrontEnd Web Developer</h3>
-    <p className="home-react">Main React</p>
+    <div className="home-dev-container">
+      <h3 className="home-dev">FrontEnd Web Developer</h3>
+      <p className="home-react">Main React</p>
+    </div>
   </div>
 );
 
